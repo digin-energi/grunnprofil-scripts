4a874efebae0491c6e58dcd206279167c5d975bd
Thomas Ranvik Eriksen <ThomasRanvik.Eriksen@lede.no>
2023-04-13 17:01:25 +0200

    Refactored Context and Namespaces for easier and better use

--- Python/cim-convert-tool/main.py
+++ Python/cim-convert-tool/main.py
@@ -8,137 +8,164 @@ parameters = {
         {
             "docType": "RD",
             "docTopic": "Base Voltage",
-            "docTitle": "DIGIN10-30-BaseVoltage_RD"
+            "docTitle": "DIGIN10-30-BaseVoltage_RD",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "RD",
             "docTopic": "Geographical Region",
-            "docTitle": "DIGIN10-30-GeographicalRegion_RD"
+            "docTitle": "DIGIN10-30-GeographicalRegion_RD",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "BM",
             "docTopic": "Boundry",
-            "docTitle": "DIGIN10-30-HV1-MV1_BM"
+            "docTitle": "DIGIN10-30-HV1-MV1_BM",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "AS",
             "docTopic": "Asset",
-            "docTitle": "DIGIN10-30-LV1_AS"
+            "docTitle": "DIGIN10-30-LV1_AS",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "CU",
             "docTopic": "Customer",
-            "docTitle": "DIGIN10-30-LV1_CU"
+            "docTitle": "DIGIN10-30-LV1_CU",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "DL",
             "docTopic": "Diagram Layout",
-            "docTitle": "DIGIN10-30-LV1_DL"
+            "docTitle": "DIGIN10-30-LV1_DL",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "EQ",
             "docTopic": "Equipment",
-            "docTitle": "DIGIN10-30-LV1_EQ"
+            "docTitle": "DIGIN10-30-LV1_EQ",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "GL",
             "docTopic": "Geographical Location",
-            "docTitle": "DIGIN10-30-LV1_GL"
+            "docTitle": "DIGIN10-30-LV1_GL",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "OP",
             "docTopic": "Operation",
-            "docTitle": "DIGIN10-30-LV1_OP"
+            "docTitle": "DIGIN10-30-LV1_OP",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "OR",
             "docTopic": "Object Reference",
-            "docTitle": "DIGIN10-30-LV1_OR"
+            "docTitle": "DIGIN10-30-LV1_OR",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "SC",
             "docTopic": "Short Circuit",
-            "docTitle": "DIGIN10-30-LV1_SC"
+            "docTitle": "DIGIN10-30-LV1_SC",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "SSH",
             "docTopic": "Steady State Hypothesis",
-            "docTitle": "DIGIN10-30-LV1_SSH"
+            "docTitle": "DIGIN10-30-LV1_SSH",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "AC",
             "docTopic": "AssetCatalogue",
-            "docTitle": "DIGIN10-30-M1_AC"
+            "docTitle": "DIGIN10-30-M1_AC",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "RD",
             "docTopic": "Measurement Value Source",
-            "docTitle": "DIGIN10-30-MeasurementValueSource_RD"
+            "docTitle": "DIGIN10-30-MeasurementValueSource_RD",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "AS",
             "docTopic": "Asset",
-            "docTitle": "DIGIN10-30-MV1_AS"
+            "docTitle": "DIGIN10-30-MV1_AS",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "CU",
             "docTopic": "Customer",
-            "docTitle": "DIGIN10-30-MV1_CU"
+            "docTitle": "DIGIN10-30-MV1_CU",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "DL",
             "docTopic": "Diagram Layout",
-            "docTitle": "DIGIN10-30-MV1_DL"
+            "docTitle": "DIGIN10-30-MV1_DL",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "EQ",
             "docTopic": "Equipment",
-            "docTitle": "DIGIN10-30-MV1_EQ"
+            "docTitle": "DIGIN10-30-MV1_EQ",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "GL",
             "docTopic": "Geographical Location",
-            "docTitle": "DIGIN10-30-MV1_GL"
+            "docTitle": "DIGIN10-30-MV1_GL",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "OP",
             "docTopic": "Operation",
-            "docTitle": "DIGIN10-30-MV1_OP"
+            "docTitle": "DIGIN10-30-MV1_OP",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "SC",
             "docTopic": "Short Circuit",
-            "docTitle": "DIGIN10-30-MV1_SC"
+            "docTitle": "DIGIN10-30-MV1_SC",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "SSH",
             "docTopic": "Steady State Hypothesis",
-            "docTitle": "DIGIN10-30-MV1_SSH"
+            "docTitle": "DIGIN10-30-MV1_SSH",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "BM",
             "docTopic": "Boundry",
-            "docTitle": "DIGIN10-30-MV1-LV1_BM"
+            "docTitle": "DIGIN10-30-MV1-LV1_BM",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "SV",
             "docTopic": "State Variables",
-            "docTitle": "DIGIN10-30-MV1-LV1_SV"
+            "docTitle": "DIGIN10-30-MV1-LV1_SV",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "TP",
             "docTopic": "Topology",
-            "docTitle": "DIGIN10-30-MV1-LV1_TP"
+            "docTitle": "DIGIN10-30-MV1-LV1_TP",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "RD",
             "docTopic": "Reading Quality Type",
-            "docTitle": "DIGIN10-30-ReadingQualityType_RD"
+            "docTitle": "DIGIN10-30-ReadingQualityType_RD",
+            "wantDiginNameSpaces": True
         },
         {
             "docType": "RD",
             "docTopic": "Reading Type",
-            "docTitle": "DIGIN10-30-ReadingType_RD"
+            "docTitle": "DIGIN10-30-ReadingType_RD",
+            "wantDiginNameSpaces": True
         }
     ]
 }
@@ -151,5 +178,6 @@ for i in range(0, len(parameters["documents"])):
     docType = parameters["documents"][i]["docType"]
     docTopic = parameters["documents"][i]["docTopic"]
     docTitle = parameters["documents"][i]["docTitle"]
+    wantDiginNameSpaces = parameters["documents"][i]["wantDiginNameSpaces"]
     
-    xmlToJsonLDConverter(companyUuid, companyName, isVersionOfUrl, docType, docTopic, docTitle)
+    xmlToJsonLDConverter(companyUuid, companyName, isVersionOfUrl, docType, docTopic, docTitle, wantDiginNameSpaces)
